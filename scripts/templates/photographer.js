56f295a7d4f06e0835d005459106981f8b493935
Jerem16 <jeremy.lemaignent@gmail.com>
2024-04-25 14:04:56 +0200

    first commit

---
+++ scripts/templates/photographer.js
@@ -0,0 +1,24 @@
+function photographerTemplate(data) {
+    const { name, portrait, city, country, tagline, price } = data;
+
+    const picture = `assets/photographers/${portrait}`;
+
+    function getUserCardDOM() {
+        const article = document.createElement("article");
+        article.innerHTML = `
+        <img
+            src= ${picture}
+            alt=${name}
+        />
+        <h2>${name}</h2>
+        <p class="location">${city}, ${country}</p>
+        <p class="tagline">${tagline}</p>
+        <p class="trj">${price}/jours</p>
+        <a class="card" href="./photographer.html" tabindex="0">link to ${name}</a>
+        `;
+
+        return article;
+    }
+
+    return {getUserCardDOM};
+}
